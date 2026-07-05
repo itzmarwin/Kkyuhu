a2e896bccbdd38c779be0a7bb6a89a4dd7b30c7c
itzmarwin <itzm4rw1n@gmail.com>
2026-07-06 03:12:41 +0530

    Update changetime.py

--- shivu/modules/changetime.py
+++ shivu/modules/changetime.py
@@ -1,10 +1,8 @@
 from pymongo import ReturnDocument
-from pyrogram.enums import ChatMemberStatus, ChatType  # Changed to pyrogram
+from pyrogram.enums import ChatMemberStatus, ChatType
 from shivu import user_totals_collection, shivuu
-from pyrogram import Client, filters  # Changed to pyrogram
-from pyrogram.types import Message  # Changed to pyrogram
-
-# Tumhara Plan: Memory cache import kar rahe hain taaki update kar saken
+from pyrogram import Client, filters
+from pyrogram.types import Message
 from shivu.__main__ import group_freq_cache
 
 ADMINS = [ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.OWNER]
@@ -12,7 +10,6 @@ ADMINS = [ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.OWNER]
 @shivuu.on_message(filters.command("changetime"))
 async def change_time(client: Client, message: Message):
     
-    # Crash fix: Agar anonymous admin ya channel post ho toh from_user None ho sakta hai
     if not message.from_user:
         await message.reply_text("Please use this command as a normal admin, not anonymous.")
         return
@@ -41,7 +38,6 @@ async def change_time(client: Client, message: Message):
             await message.reply_text('The message frequency must be greater than or equal to 100.')
             return
 
-        # MongoDB Update
         await user_totals_collection.find_one_and_update(
             {'chat_id': str(chat_id)},
             {'$set': {'message_frequency': new_frequency}},
@@ -49,7 +45,6 @@ async def change_time(client: Client, message: Message):
             return_document=ReturnDocument.AFTER
         )
 
-        # TUMHARA PLAN: Memory Cache Update
         group_freq_cache[str(chat_id)] = new_frequency
 
         await message.reply_text(f'Successfully changed drop frequency to {new_frequency} messages. Next cycle will use this new limit!')
