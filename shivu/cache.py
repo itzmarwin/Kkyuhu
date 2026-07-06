797242991d9621106fb0958f4dc9bd3e47af85de
itzmarwin <itzm4rw1n@gmail.com>
2026-07-07 02:22:45 +0530

    Update and rename tt.py to cache.py

---
+++ shivu/cache.py
@@ -0,0 +1,12 @@
+all_characters_cache = []
+characters_by_id = {}
+group_freq_cache = {}
+
+locks = {}
+message_counts = {}
+last_characters = {}
+sent_characters = {}
+first_correct_guesses = {}
+
+last_user = {}
+warned_users = {}
